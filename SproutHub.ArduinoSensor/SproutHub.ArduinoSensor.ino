1b3a5c0f5b493642dbad0521288961ac61102816
zmozel <zmozel@spotsolutions.com>
2026-04-30 17:53:45 -0400

    multiple sensors

--- SproutHub.ArduinoSensor/SproutHub.ArduinoSensor.ino
+++ SproutHub.ArduinoSensor/SproutHub.ArduinoSensor.ino
@@ -1,13 +1,19 @@
 const unsigned long READ_INTERVAL_MS = 5UL * 60UL * 1000UL; // 5 min
+const int ID_SEED = 1001;
+const int SENSOR_COUNT = 2;
 
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  int moisture = analogRead(A0);
-  Serial.print("|1001|");
-  Serial.print(moisture);
-  Serial.println("|");
+  for (int i = 0;i < SENSOR_COUNT;i++) {
+    int moisture = analogRead(A0 + 1);
+    Serial.print("|");
+    Serial.print(ID_SEED + i);
+    Serial.print("|");
+    Serial.print(moisture);
+    Serial.println("|");
+  }
   delay(READ_INTERVAL_MS);
 }
